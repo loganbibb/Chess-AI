bda091d33d3e82a8be717f28e55c1d04136b20a3
loganbibb <90297615+loganbibb@users.noreply.github.com>
2026-07-12 22:53:53 -0700

    Updated type of offsets
    Now uses inline constexpr, which will reduce to a single instance after compilation / linking. Old method would create an instance every time header is included.
    
    Also updated to array type, as we don't need dynamic allocation here.

--- src/include/move_offsets.h
+++ src/include/move_offsets.h
@@ -1,25 +1,25 @@
 #pragma once 
 
-#include <vector>
+#include <array>
 
 namespace offsets {
     // knight move offsets
-    static const std::vector<std::pair<int, int>> knight_move_offsets = {
+    inline constexpr std::array<std::pair<int, int>, 8> knight_move_offsets = {{
         {1, 2}, {2, 1}, {2, -1}, {1, -2}, 
         {-1, -2}, {-2, -1}, {-2, 1}, {-1, 2}
-    };
+    }};
     // bishop move offsets
-    static const std::vector<std::pair<int, int>> bishop_move_offsets = {
+    inline constexpr std::array<std::pair<int, int>, 4> bishop_move_offsets = {{
         {1, 1}, {1, -1}, {-1, -1}, {-1, 1}
-    };
+    }};
     // rook move offsets
-    static const std::vector<std::pair<int, int>> rook_move_offsets = {
+    inline constexpr std::array<std::pair<int, int>, 4> rook_move_offsets = {{
         {1, 0}, {-1, 0}, {0, 1}, {0, -1}
-    };
+    }};
     // king move offsets
-    static const std::vector<std::pair<int, int>> king_move_offsets = {
+    inline constexpr std::array<std::pair<int, int>, 8> king_move_offsets = {{
         {1, 1}, {1, 0}, {1, -1}, 
         {0, -1}, {-1, -1}, {-1, 0}, 
         {-1, 1}, {0, 1}
-    };
+    }};
 }
