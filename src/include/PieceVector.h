c13f3841a8476a3b90c16af9c19b3dff6842f078
loganbibb <90297615+loganbibb@users.noreply.github.com>
2026-09-19 22:11:35 -0700

    Add evaluation.cpp; convert PieceVector to std::array

--- src/include/PieceVector.h
+++ src/include/PieceVector.h
@@ -1,6 +1,7 @@
 #pragma once 
 
-#include <vector> 
+#include <array> 
+#include <vector>
 #include <string>
 #include <stdint.h>
 
@@ -8,14 +9,16 @@
 #include "Piece.h"
 #include "Square.h"
 
-class PieceVector : public std::vector<Piece> {
+class PieceVector : public std::array<Piece, 64> {
     /*
     PieceVector is a wrapper for a vector of pieces. It is used to represent the pieces on the board in a position. 
     It is essentially a vector of 64 pieces, where each piece represents the piece on the corresponding square. 
     This class can be used to easily convert between a vector of pieces and a string representation of the board.
     */
     public:
-    PieceVector() : std::vector<Piece>(64, Piece::NOPIECE) {} // initialize all squares to NOPIECE
+    PieceVector() : std::array<Piece, 64>() {
+        this->fill(Piece::NOPIECE); // initialize all squares to NOPIECE
+    } 
     std::string to_string();
     Piece& operator()(int8_t file, int8_t rank);
     Piece& operator()(const Square& square);
@@ -25,6 +28,6 @@ class PieceVector : public std::vector<Piece> {
         if (index < 0 || index >= 64) {
             throw Exceptions::IndexError();
         }
-        return std::vector<Piece>::operator[](index);
+        return std::array<Piece,64>::operator[](index);
     }
 };
